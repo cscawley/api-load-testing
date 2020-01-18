ba1470b20da178da2fed6b0dc2333999374d69e6
Chris Cawley <5122906+cscawley@users.noreply.github.com>
2020-01-17 19:33:37 -0600

    cleanup

--- index.js
+++ index.js
@@ -6,23 +6,11 @@ var config = {
     hits: 50,
     parallel: 10
 }
-/**
- * A set of collection run options for the paralle collection runs. For demonstrative purposes in this script, an
- * identical set of options has been used. However, different options can be used, so as to actually run different
- * collections, with their corresponding run options in parallel.
- *
- * @type {Object}
- */
 options = {
     collection: path.join(__dirname, 'run.json'),
     reporters: 'cli',
     iterationCount: config.hits
 },
-/**
- * A collection runner function that runs a collection for a pre-determined options object.
- *
- * @param {Function} done - A callback function that marks the end of the current collection run, when called.
- */
 parallelCollectionRun = function (done) {
     newman.run(options, done);
 };
@@ -30,21 +18,12 @@ var addarray = [];
 for (let run = 0; run < config.parallel; run++) {
     addarray.push(parallelCollectionRun)
 }
-// Runs the Postman sample collection thrice, in parallel.
+// Runs the Postman sample collection, in parallel.
 async.parallel(addarray,
-/**
- * The
- *
- * @param {?Error} err - An Error instance / null that determines whether or not the parallel collection run
- * succeeded.
- * @param {Array} result - An array of collection run summary objects.
- */
     function (err, result) {
     err && console.error(err);
     result.forEach(function (result) {
         var failures = result.run.failures;
-
-        console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
-            `${result.collection.name} ran successfully.`);
+        console.info(failures.length ? JSON.stringify(failures.failures, null, 2) : `${result.collection.name} ran successfully.`);
     });
 });
